d5a2afb7e88d37946979d931da55d28d8d0355d6
Massimiliano Pippi <mpippi@gmail.com>
2023-12-29 19:19:00 +0100

    add draw endpoint

--- src/hayhooks/server/handlers/__init__.py
+++ src/hayhooks/server/handlers/__init__.py
@@ -1,6 +1,7 @@
 from hayhooks.server.handlers import deploy
+from hayhooks.server.handlers import draw
 from hayhooks.server.handlers import status
 from hayhooks.server.handlers import undeploy
 
 
-__all__ = ["deploy", "status", "undeploy"]
+__all__ = ["deploy", "draw", "status", "undeploy"]
---
+++ src/hayhooks/server/handlers/draw.py
@@ -0,0 +1,18 @@
+import tempfile
+from pathlib import Path
+
+from fastapi import HTTPException
+from fastapi.responses import FileResponse
+from hayhooks.server import app
+from hayhooks.server.pipelines import registry
+
+
+@app.get("/draw/{pipeline_name}")
+async def status(pipeline_name):
+    pipeline = registry.get(pipeline_name)
+    if not pipeline:
+        raise HTTPException(status_code=404)
+
+    _, fpath = tempfile.mkstemp()
+    pipeline.draw(Path(fpath))
+    return FileResponse(fpath, media_type="image/png")
