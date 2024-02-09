ffb8ccf3263753113fdc38bb2b1bb409d86af8af
anakin87 <stefanofiorucci@gmail.com>
2024-02-09 12:56:32 +0100

    change run arguments to options

--- src/hayhooks/cli/run/__init__.py
+++ src/hayhooks/cli/run/__init__.py
@@ -3,7 +3,7 @@ import uvicorn
 
 
 @click.command()
-@click.argument("host", default="localhost")
-@click.argument('port', default=1416)
+@click.option('-h', '--host', default="localhost")
+@click.option('-p', '--port', default=1416)
 def run(host, port):
     uvicorn.run("hayhooks.server:app", host=host, port=port)
