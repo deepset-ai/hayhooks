beb59d5db5eb9c7c331b46e6a8cdf39c2f3e6ea2
Massimiliano Pippi <mpippi@gmail.com>
2024-02-20 17:11:45 +0100

    fix env var reading at startup

--- src/hayhooks/cli/run/__init__.py
+++ src/hayhooks/cli/run/__init__.py
@@ -6,7 +6,9 @@ import os
 @click.command()
 @click.option('--host', default="localhost")
 @click.option('--port', default=1416)
-@click.option('--pipelines-dir', default="pipelines.d")
+@click.option('--pipelines-dir', default=os.environ.get("HAYHOOKS_PIPELINES_DIR"))
 def run(host, port, pipelines_dir):
+    if not pipelines_dir:
+        pipelines_dir = "pipelines.d"
     os.environ["HAYHOOKS_PIPELINES_DIR"] = pipelines_dir
     uvicorn.run("hayhooks.server:app", host=host, port=port)
