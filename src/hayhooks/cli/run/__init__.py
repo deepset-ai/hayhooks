e7f5a275d7ed6e5a783caed99be1990d4090e8f5
Michele Pangrazzi <xmikex83@gmail.com>
2025-01-14 14:07:46 +0100

    add missing settings

--- src/hayhooks/cli/run/__init__.py
+++ src/hayhooks/cli/run/__init__.py
@@ -8,11 +8,13 @@ from hayhooks.settings import settings
 @click.option('--host', default=settings.host)
 @click.option('--port', default=settings.port)
 @click.option('--pipelines-dir', default=settings.pipelines_dir)
+@click.option('--root-path', default=settings.root_path)
 @click.option('--additional-python-path', default=settings.additional_python_path)
-def run(host, port, pipelines_dir, additional_python_path):
+def run(host, port, pipelines_dir, root_path, additional_python_path):
     settings.host = host
     settings.port = port
     settings.pipelines_dir = pipelines_dir
+    settings.root_path = root_path
     settings.additional_python_path = additional_python_path
 
     if additional_python_path:
