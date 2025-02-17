b9139fcd92c1a89f2e8e6edaf704f225335356fd
Michele Pangrazzi <xmikex83@gmail.com>
2025-02-17 17:15:52 +0100

    Run hayhooks programmatically (#77)
    * Export create_app from hayhooks
    
    * Update README with example script

--- src/hayhooks/__init__.py
+++ src/hayhooks/__init__.py
@@ -1,4 +1,5 @@
 from hayhooks.server.logger import log
+from hayhooks.server.app import create_app
 from hayhooks.server.utils.base_pipeline_wrapper import BasePipelineWrapper
 from hayhooks.server.pipelines.utils import (
     is_user_message,
@@ -12,4 +13,5 @@ __all__ = [
     "is_user_message",
     "get_last_user_message",
     "streaming_generator",
+    "create_app",
 ]
