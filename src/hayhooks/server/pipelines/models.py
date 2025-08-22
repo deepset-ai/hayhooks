0274d2239c2219581cdb3d95517557a22e8575bf
anakin87 <stefanofiorucci@gmail.com>
2025-08-22 12:05:58 +0200

    models improvements

--- src/hayhooks/server/pipelines/models.py
+++ src/hayhooks/server/pipelines/models.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Union, Dict, List, Any
 
 from haystack import Document
 from pydantic import BaseModel, ConfigDict, create_model
@@ -16,7 +16,7 @@ DEFAULT_TYPES_MAPPING = {
 }
 
 
-def get_request_model(pipeline_name: str, pipeline_inputs: dict[str, Any]) -> type[BaseModel]:
+def get_request_model(pipeline_name: str, pipeline_inputs: Dict[str, Dict[str, Any]]) -> type[BaseModel]:
     """
     Inputs have the form below.
 
@@ -28,28 +28,29 @@ def get_request_model(pipeline_name: str, pipeline_inputs: dict[str, Any]) -> ty
         'second_addition': {'add': {'type': typing.Optional[int], 'is_mandatory': False}},
     }
     """
-    request_model: dict[str, Any] = {}
+    request_model: Dict[str, Any] = {}
     config = ConfigDict(arbitrary_types_allowed=True)
 
     for component_name, inputs in pipeline_inputs.items():
-        component_model: dict[str, Any] = {}
+        component_model: Dict[str, Any] = {}
         for name, typedef in inputs.items():
-            try:
-                input_type = handle_unsupported_types(type_=typedef["type"], types_mapping=DEFAULT_TYPES_MAPPING)
-            except TypeError as e:
-                raise e
+            if isinstance(typedef, dict) and "type" in typedef:
+                try:
+                    input_type = handle_unsupported_types(type_=typedef["type"], types_mapping=DEFAULT_TYPES_MAPPING)
+                except TypeError as e:
+                    raise e
 
-            if input_type is not None:
-                component_model[name] = (
-                    input_type,
-                    typedef.get("default_value", ...),
-                )
+                if input_type is not None:
+                    component_model[name] = (
+                        input_type,
+                        typedef.get("default_value", ...),
+                    )
         request_model[component_name] = (create_model("ComponentParams", **component_model, __config__=config), ...)
 
     return create_model(f"{pipeline_name.capitalize()}RunRequest", **request_model, __config__=config)
 
 
-def get_response_model(pipeline_name: str, pipeline_outputs: dict[str, Any]) -> type[BaseModel]:
+def get_response_model(pipeline_name: str, pipeline_outputs: Dict[str, Dict[str, Any]]) -> type[BaseModel]:
     """
     Outputs have the form below.
 
@@ -59,22 +60,23 @@ def get_response_model(pipeline_name: str, pipeline_outputs: dict[str, Any]) -> 
         },
     }
     """
-    response_model: dict[str, Any] = {}
+    response_model: Dict[str, Any] = {}
     config = ConfigDict(arbitrary_types_allowed=True)
     for component_name, outputs in pipeline_outputs.items():
-        component_model: dict[str, Any] = {}
+        component_model: Dict[str, Any] = {}
         for name, typedef in outputs.items():
-            output_type = typedef["type"]
-            component_model[name] = (
-                handle_unsupported_types(type_=output_type, types_mapping=DEFAULT_TYPES_MAPPING),
-                ...,
-            )
+            if isinstance(typedef, dict) and "type" in typedef:
+                output_type = typedef["type"]
+                component_model[name] = (
+                    handle_unsupported_types(type_=output_type, types_mapping=DEFAULT_TYPES_MAPPING),
+                    ...,
+                )
         response_model[component_name] = (create_model("ComponentParams", **component_model, __config__=config), ...)
 
     return create_model(f"{pipeline_name.capitalize()}RunResponse", **response_model, __config__=config)
 
 
-def convert_value_to_dict(value: Any) -> Any:
+def convert_value_to_dict(value: Any) -> Union[str, int, float, bool, None, Dict[str, Any], List[Any]]:
     """Convert a single value to a dictionary if possible"""
     if hasattr(value, "to_dict"):
         if "init_parameters" in value.to_dict():
@@ -90,7 +92,7 @@ def convert_value_to_dict(value: Any) -> Any:
         return value
 
 
-def convert_component_output(component_output: Any) -> Any:
+def convert_component_output(component_output: Any) -> Union[str, int, float, bool, None, Dict[str, Any], List[Any]]:
     """
     Converts component outputs to dictionaries that can be validated against response model.
 
