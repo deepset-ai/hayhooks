371f359530456febce50f23ab9dc65ab256ef069
Michele Pangrazzi <xmikex83@gmail.com>
2026-06-04 10:50:55 +0200

    Using dark theme + vivid colors by default

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,4 +1,6 @@
-import '@fontsource/inter'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
